362bf1f3baf748bf4e35e17692b035530677f7ae
Maxine Ellah <maxine.ellah@gmail.com>
2016-07-07 18:19:12 +1200

    make artinfo directive file

--- controller/artCtrl.js
+++ controller/artCtrl.js
@@ -1,5 +1,27 @@
 app.controller('artCtrl', function($scope) {
-  $scope.art = {
-    
+  $scope.madonna = {
+    name: 'madonna',
+    image: './images/madonna.jpg'
+  };
+
+  $scope.untitled = {
+    name: 'untitled',
+    image: './images/Untitled-1.jpg'
+  };
+
+  $scope.womanwave = {
+    name: 'womanwave',
+    image: './images/womanwave.jpg'
+  };
+
+  $scope.lines = {
+    name: 'blackerlines',
+    image: './images/blackerlinessmall.jpg'
+  };
+
+  $scope.chosen = {
+    name: 'chosen',
+    image: './images/chosen3.jpg'
   }
+
 })
