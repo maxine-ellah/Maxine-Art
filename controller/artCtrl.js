6b371a602b4df4c5773105dd63a7255f9f2976fd
Maxine Ellah <maxine.ellah@gmail.com>
2016-07-23 15:50:22 +1200

    add gallery property to artCtrl.js so that the art-info directive displays all images

--- controller/artCtrl.js
+++ controller/artCtrl.js
@@ -1,32 +1,67 @@
 app.controller('artCtrl', function($scope) {
   $scope.prince = {
     name: "black",
-    image: "./images/black.jpg"
+    images: [
+      './images/black.jpg'
+    ]
   };
 
-  $scope.madonna = {
-    name: 'madonna',
-    image: './images/madonna.jpg'
-  };
+  $scope.gallery = [
+    {
+      name: 'madonna',
+      images: './images/madonna.jpg'
+    },
+    {
+      name: 'untitled',
+      images: './images/Untitled-1.jpg'
+    },
+    {
+      name: 'womanwave',
+      images: './images/womanwave.jpg'
+    },
+    {
+      name: 'blackerlines',
+      images: './images/blackerlinessmall.jpg'
+    },
+    {
+      name: 'chosen',
+      images: './images/chosen3.jpg'
+    }
+  ];
 
-  $scope.untitled = {
-    name: 'untitled',
-    image: './images/Untitled-1.jpg'
-  };
-
-  $scope.womanwave = {
-    name: 'womanwave',
-    image: './images/womanwave.jpg'
-  };
-
-  $scope.lines = {
-    name: 'blackerlines',
-    image: './images/blackerlinessmall.jpg'
-  };
-
-  $scope.chosen = {
-    name: 'chosen',
-    image: './images/chosen3.jpg'
-  }
+  // $scope.madonna = {
+  //   name: 'madonna',
+  //   images: [
+  //     './images/madonna.jpg'
+  //   ]
+  // };
+  //
+  // $scope.untitled = {
+  //   name: 'untitled',
+  //   images: [
+  //     './images/Untitled-1.jpg'
+  //   ]
+  // };
+  //
+  // $scope.womanwave = {
+  //   name: 'womanwave',
+  //   images: [
+  //     './images/womanwave.jpg'
+  //   ]
+  // };
+  //
+  // $scope.lines = {
+  //   name: 'blackerlines',
+  //   images: [
+  //     './images/blackerlinessmall.jpg'
+  //   ]
+  // };
+  //
+  // $scope.chosen = {
+  //   name: 'chosen',
+  //   images: [
+  //     './images/chosen3.jpg'
+  //   ]
+  // }
 
 })
