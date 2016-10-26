2d9ec490fea9008a3dae11961ec88b928b3b9a00
Maxine Ellah <maxine.ellah@gmail.com>
2016-10-26 20:15:21 +1300

    try to fix script babelify error

---
+++ public/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Maxine Ellah</h1>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <App />
+  document.getElementById('content')
+);
