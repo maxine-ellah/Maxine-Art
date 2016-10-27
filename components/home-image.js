d8cf3909c3586815bdc47e90a1bb36b268136047
Maxine Ellah <maxine.ellah@gmail.com>
2016-10-27 16:32:27 +1300

    set up components

---
+++ components/home-image.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class homeImage extends Component {
+  componentWillMount()
+}
+
+render() {
+  return (
+    <div>
+      <img src={} onClick={}
+    </div>
+  )
+}
+
+export default homeImage;
