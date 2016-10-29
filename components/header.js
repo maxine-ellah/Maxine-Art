dfc8cd96e1fcaa6216306fe36bfe17d1ec01794e
Maxine Ellah <maxine.ellah@gmail.com>
2016-10-29 15:44:11 +1300

    make header component

---
+++ components/header.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <div className="Header">
+        <p>Maxine Ellah</p>
+      </div>
+    )
+  }
+}
+
+
+
+export default Header;
