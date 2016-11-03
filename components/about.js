21ecef37641276f9bc6714ff5ac99f913ce270b2
Maxine Ellah <maxine.ellah@gmail.com>
2016-11-04 11:32:57 +1300

    make about and contact components

---
+++ components/about.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <p>A developer</p>
+    )
+  }
+}
+
+export default About;
---
+++ components/contact.js
@@ -0,0 +1,15 @@
+import React, {Component} fro 'react';
+
+class Contact extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <p>maxine.ellah@gmail.com</p>
+    )
+  }
+}
+
+export default Contact;
