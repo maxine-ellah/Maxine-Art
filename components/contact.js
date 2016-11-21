220c7352acd3486798385ff04eeed7e4320fd2e3
Maxine Ellah <maxine.ellah@gmail.com>
2016-11-21 14:08:35 +1300

    about and contact content centered in desktop view

--- components/contact.js
+++ components/contact.js
@@ -8,7 +8,7 @@ class Contact extends Component {
   render(){
     return (
       <div className="col-4 contact-content">
-        <a className="email" href="http://www.shannonmaypowell.com/">maxine.ellah@gmail.com</a>
+        <a className="email" href="mailto:maxine.ellah@gmail.com">maxine.ellah@gmail.com</a>
         <a className="twitter" href="https://twitter.com/_ndidi">@_ndidi</a>
       </div>
     )
