8ad7138d28bd9dee8890a9b00a52a171c9d2232b
Maxine Ellah <maxine.ellah@gmail.com>
2016-11-23 13:23:53 +1300

    change file path to index.html in server.js

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ let app = express();
 app.use(express.static('public'));
 
 app.get('/contact', function(req, res){
-  res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile('index.html', { root: __dirname });
 })
 
 app.get('/about', function(req, res){
-  res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile('index.html', { root: __dirname });
 })
 
 app.get('*', function(req, res){
-  res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile('index.html', { root: __dirname });
 })
 
 app.listen(8080, function(){
