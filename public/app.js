b621a886462a1534cb38737d8d49cc0868262084
Maxine Ellah <maxine.ellah@gmail.com>
2016-11-28 11:49:45 +1300

    add hashHistory to app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import {Router, Route, browserHistory, hashHistory, IndexRoute} from 'react-router';
 //Import custom components
 import About from '../components/about.js';
 import Contact from '../components/contact.js';
@@ -9,8 +9,8 @@ import Imagelist from '../components/image-list.js';
 
 
   render(
-      <Router history={browserHistory}>
-        <Route path="/Maxine-Art/" component={Sidebar}>
+      <Router history={hashHistory}>
+        <Route path="/" component={Sidebar}>
           <IndexRoute component={Imagelist}/>
           <Route path="/about" component={About}/>
           <Route path="/contact" component={Contact}/>
