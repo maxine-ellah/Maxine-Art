d6d8322913accfb7bbefe96d5f9ec5915446d451
Maxine Ellah <maxine.ellah@gmail.com>
2016-12-05 15:59:28 +1300

    remove react-router from sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Router, Route, Link } from 'react-router';
 //Import custom components
 import About from '../components/about.js';
 import Contact from '../components/contact.js';
