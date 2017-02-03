0944303000b2813a89e11fdae0a7687bb3c0bdfe
Maxine Ellah <maxine.ellah@gmail.com>
2017-02-04 10:16:36 +1300

    add target blank so link opens in new tab

--- components/contact.js
+++ components/contact.js
@@ -8,8 +8,8 @@ class Contact extends Component {
   render(){
     return (
       <div className="col-4 contact-content">
-        <a href="mailto:maxine.ellah@gmail.com">maxine.ellah@gmail.com</a>
-        <a href="http://kiiis.tumblr.com/">kiiis.tumblr.com</a>
+        <a href="mailto:maxine.ellah@gmail.com" target="_blank">maxine.ellah@gmail.com</a>
+        <a href="http://kiiis.tumblr.com/" target="_blank">kiiis.tumblr.com</a>
       </div>
     )
   }
