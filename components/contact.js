7b7fad7fdb0d6655f4e35faa33b67f3cf3cfafff
Maxine Ellah <maxine.ellah@gmail.com>
2017-02-04 10:40:02 +1300

    change kiiis to tumblr

--- components/contact.js
+++ components/contact.js
@@ -9,7 +9,7 @@ class Contact extends Component {
     return (
       <div className="col-4 contact-content">
         <a href="mailto:maxine.ellah@gmail.com" target="_blank">maxine.ellah@gmail.com</a>
-        <a href="http://kiiis.tumblr.com/" target="_blank">kiiis.tumblr.com</a>
+        <a href="http://kiiis.tumblr.com/" target="_blank">tumblr</a>
       </div>
     )
   }
